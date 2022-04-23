d297bc05e10a843514d04059b71c0ed2fb138328
jfelya <fely756@gmail.com>
2022-04-23 15:36:36 -0400

    Writing

--- writing_files.py
+++ writing_files.py
@@ -1,1 +1,5 @@
-print("Uga uga")
+# Open a file with access mode 'a'
+with open("employees2.txt", "a+") as file_object:
+    # Append 'hello' at the end of file
+    file_object.write("\nhello")
+    print(file_object.read())
