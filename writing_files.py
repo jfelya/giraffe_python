8a12261c5733d91369e9a811d9a7e1f3cc057ff8
jfelya <fely756@gmail.com>
2022-04-23 16:14:30 -0400

    Final commit for the day

--- writing_files.py
+++ writing_files.py
@@ -1,5 +1,5 @@
 # Open a file with access mode 'a'
-# w would overwrite the entire text
+# w would overwrite the entire text, if the file doesn't exist, it will create it
 with open("employees2.txt", "a+") as file_object:
     # Append 'hello' at the end of file
     text_insert = input("Type the text you want to append to the file: ")
