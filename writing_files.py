e7942e2d75a6dad9c098baf726b90d30bf463cc0
jfelya <fely756@gmail.com>
2022-04-23 15:47:23 -0400

    Changes to writing

--- writing_files.py
+++ writing_files.py
@@ -1,5 +1,11 @@
 # Open a file with access mode 'a'
+# w would overwrite the entire text
 with open("employees2.txt", "a+") as file_object:
     # Append 'hello' at the end of file
-    file_object.write("\nhello")
-    print(file_object.read())
+    text_insert = input("Type the text you want to append to the file: ")
+    text_insert2 = "\n" + text_insert
+    file_object.write(text_insert2)
+
+with open("employees2.txt", "r") as file_object:
+    for line in file_object:
+        print(line.strip())
