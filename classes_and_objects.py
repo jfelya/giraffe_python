27ea03424337a7a57050ea88e7fa348472842377
jfelya <fely756@gmail.com>
2022-05-03 18:05:32 -0400

    JDKFJ

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -5,4 +5,35 @@ class Student:
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
-        
+
+    def honor(self):
+        if self.gpa > 3.5:
+            return True
+        else:
+            return False
+
+
+estudiante = Student("Fly", "Informatica", 3.6, True)
+print(estudiante.honor())
+
+
+class Chef:
+
+    def make_dish(self):
+        print("The chef is making some steak")
+
+
+class ChineseChef(Chef):
+    # Inheriting
+
+    # Overwriting existing method
+    def make_dish(self):
+        print("The chinese chef is making some steak")
+
+    def make_fried_rice(self):
+        print("The chef is making some fried rice")
+
+
+qing = ChineseChef()
+qing.make_dish()
+qing.make_fried_rice()
